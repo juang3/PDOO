a6262f63a1b3e146f5c638d756802bb8b6f408b2
juang3 <juang3@ei142094>
2015-02-19 12:35:45 +0100

    commit inicial, probando

---
+++ Prueba1_JavaApplication1/src/prueba1_javaapplication1/Prueba1_JavaApplication1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1_javaapplication1;
+
+/**
+ *
+ * @author juang3
+ */
+public class Prueba1_JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
